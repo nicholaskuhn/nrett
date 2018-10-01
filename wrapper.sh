d33bd4cf1b659a4f816910203c4bfb9a1f755fd0
Brett Cloward <brettcloward@mail.weber.edu>
2018-10-01 15:08:04 +0000

    wrapper.sh

---
+++ wrapper.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -
+#===============================================================================
+#
+#          FILE: wrapper.sh
+#
+#         USAGE: ./wrapper.sh
+#
+#   DESCRIPTION: 
+#
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: Brett Cloward (bcloward), brettcloward@mail.weber.edu
+#  ORGANIZATION: WSU
+#       CREATED: 10/01/2018 03:05:46 PM
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                                  # Treat unset variables as an error
+
