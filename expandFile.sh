ef21b81800c7d7c829336b4bd3d5943a90748f22
Brett Cloward <brettcloward@mail.weber.edu>
2018-10-10 22:54:43 +0000

    Updated 10/10/18

--- expandFile.sh
+++ expandFile.sh
@@ -22,7 +22,8 @@ set -o nounset                                  # Treat unset variables as an er
 mkdir temp
 
 # expand all .tar.gz files that were retreived from the wget script and
-# save them to one file in a temp directory. Log your activity. 
+# save them to a temp directory. The output from the csv files
+# will be saved in a temp.txt file in the nrett directory. Log your activity. 
 for f in *.tar.gz;
 do
     tar -xvzf "$f"
